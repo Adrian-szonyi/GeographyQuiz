8340eac174c904e604182ed6a968c874bef239a3
unknown <aszonyi49@gmail.com>
2021-09-16 12:04:08 +1000

    SCores and initials are now printed on the page

--- assets/js/Highscorejs.js
+++ assets/js/Highscorejs.js
@@ -4,13 +4,15 @@ var stores = Array();
 var play = document.getElementById("Play");
 var highscoreslist = document.getElementById("highscoreslist")
 var key1 = localStorage.getItem("key");
+var scorecard = document.getElementById("scorecard");
+var savedScore = localStorage.getItem("key")
 
-// console.log(highscore);
 
+scorecard.textContent = savedScore;
 SubmitScore.addEventListener("click", () => {
     var stringToSave = inputString.value;
-       console.log(stringToSave);
-       if ((stringToSave == null) || (stringToSave == "")) {
+    console.log(stringToSave);
+    if ((stringToSave == null) || (stringToSave == "")) {
         document.getElementById('write').innerHTML = "nothing to store.";
     } else {
         stores.push(stringToSave);
@@ -18,30 +20,21 @@ SubmitScore.addEventListener("click", () => {
         window.localStorage.setItem("Initials", JSON.stringify(stringToSave));
         document.getElementById('write').innerHTML = "Thank You";
     }
-});
-    // localStorage.getItem('database')
-//     var Initials = JSON.parse(localStorage.getItem("stringToSave")) || [];
-//     highscoreslist.textContent = highscore.map(score => {
-//     return `<li class="high-score">${Initials} - ${key}</li>`;
-//   });
-// .join("");
-// const allKeys = Object.keys(localstorage);
-// return  allKeys.map(key => {localstorage.getItem(key)} );
 
+    var savedInitials = localStorage.getItem("Initials")
+    var savedScore = localStorage.getItem("key")
+    var node = document.createElement("li");
+var textnode = document.createTextNode(savedInitials + " " + savedScore);     
+node.appendChild(textnode);                              
+document.getElementById("highscoreslist").appendChild(node); 
+
+});
 
 
 localStorage.getItem("Initials");
-let key3 = localStorage.getItem("key");
+var key3 = localStorage.getItem("key");
 
 
-// });
-// function Printhighscorelist () {
-// var highscore = JSON.parse(localStorage.getItem("Initials")) || [];
-// var initials = localStorage.getItem*("database") || [];
-// const li = document.createElement("li");
-//  li.textContent = highscore;
-//  li.textContent = initials;
-// }
 
 play.addEventListener("click", () => {
     window.location.href = "./StartPage.html";
