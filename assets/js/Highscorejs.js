875c1d42889c5f506ae8a3890111fddd1cd5783d
unknown <aszonyi49@gmail.com>
2021-09-16 18:20:47 +1000

    Updated the highscore page and added comments to JS

--- assets/js/Highscorejs.js
+++ assets/js/Highscorejs.js
@@ -2,41 +2,46 @@ var SubmitScore = document.getElementById("submitscore-btn");
 var inputString = document.getElementById("inputString");
 var stores = Array();
 var play = document.getElementById("Play");
-var highscoreslist = document.getElementById("highscoreslist")
+var highscoreslist = document.getElementById("highscoreslist");
 var key1 = localStorage.getItem("key");
 var scorecard = document.getElementById("scorecard");
-var savedScore = localStorage.getItem("key")
+var savedScore = localStorage.getItem("key");
+
+var savedInitials = localStorage.getItem("Initials");
+var savedScore = localStorage.getItem("key");
+var node = document.createElement("li");
+var textnode = document.createTextNode(savedInitials + " " + savedScore);
+node.appendChild(textnode);
+document.getElementById("highscoreslist").appendChild(node);
 
 
+//saving initials to the local storage
 scorecard.textContent = savedScore;
 SubmitScore.addEventListener("click", () => {
-    var stringToSave = inputString.value;
-    console.log(stringToSave);
-    if ((stringToSave == null) || (stringToSave == "")) {
-        document.getElementById('write').innerHTML = "nothing to store.";
-    } else {
-        stores.push(stringToSave);
-        inputString.value = "";
-        window.localStorage.setItem("Initials", JSON.stringify(stringToSave));
-        document.getElementById('write').innerHTML = "Thank You";
-    }
+  var stringToSave = inputString.value;
+  console.log(stringToSave);
+  if (stringToSave == null || stringToSave == "") {
+    document.getElementById("write").innerHTML = "nothing to store.";
+  } else {
+    stores.push(stringToSave);
+    inputString.value = "";
+    window.localStorage.setItem("Initials", JSON.stringify(stringToSave));
+    document.getElementById("write").innerHTML = "Thank You";
+  }
 
-    var savedInitials = localStorage.getItem("Initials")
-    var savedScore = localStorage.getItem("key")
-    var node = document.createElement("li");
-var textnode = document.createTextNode(savedInitials + " " + savedScore);     
-node.appendChild(textnode);                              
-document.getElementById("highscoreslist").appendChild(node); 
 
+  //pulling objects from the local storage
+  var savedInitials = localStorage.getItem("Initials");
+  var savedScore = localStorage.getItem("key");
+  var node = document.createElement("li");
+  var textnode = document.createTextNode(savedInitials + " " + savedScore);
+  node.appendChild(textnode);
+  document.getElementById("highscoreslist").appendChild(node);
 });
-
 
 localStorage.getItem("Initials");
 var key3 = localStorage.getItem("key");
 
-
-
 play.addEventListener("click", () => {
-    window.location.href = "./StartPage.html";
-
+  window.location.href = "./StartPage.html";
 });
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,48 +1,46 @@
 var Quizquestions = [
-    {
-        question: "When was Javascript invented?",
-           a: "1992",
-           b: "1993",
-           c: "1995",
-           d: "1998",
+  {
+    question: "When was Javascript invented?",
+    a: "1992",
+    b: "1993",
+    c: "1995",
+    d: "1998",
     correctanswer: "c",
-    },
-    {
-        question: "What's the most popular coding language in 2021?",
-        a: "Python",
-        b: "C#",
-        c: "Javascript",
-        d: "Java",
- 
-     correctanswer: "a",
-    },
-    {
-        question: "What is an array?",
-           a: "An indexed collection of elements",
-           b: "A CSS selector",
-           c: "HTML code",
-           d: "A beam of sunlight",
-    
-        correctanswer: "a",
-    },
-    {
-        question: "Who invented Javascript?",
-        a: "Lisa Simpson",
-        b: "Jerry Seinfeld",
-        c: "Thomas Jefferson",
-        d: "Brendan Eich",
- 
-     correctanswer: "d",
-    },
-];
+  },
+  {
+    question: "What's the most popular coding language in 2021?",
+    a: "Python",
+    b: "C#",
+    c: "Javascript",
+    d: "Java",
 
+    correctanswer: "a",
+  },
+  {
+    question: "What is an array?",
+    a: "An indexed collection of elements",
+    b: "A CSS selector",
+    c: "HTML code",
+    d: "A beam of sunlight",
+
+    correctanswer: "a",
+  },
+  {
+    question: "Who invented Javascript?",
+    a: "Lisa Simpson",
+    b: "Jerry Seinfeld",
+    c: "Thomas Jefferson",
+    d: "Brendan Eich",
+
+    correctanswer: "d",
+  },
+];
 
 var aanswer = document.getElementById("a1");
 var banswer = document.getElementById("b1");
 var canswer = document.getElementById("c1");
 var danswer = document.getElementById("d1");
 var questionEl = document.getElementById("question");
-// var Startbtn = document.getElementById("start-quiz");
 var submitbutton = document.getElementById("submit-btn");
 var timer = document.getElementById("timer");
 var answers = document.querySelectorAll(".answer1");
@@ -52,14 +50,13 @@ var score = 0;
 var timeleft = 40;
 var currentselectedanswer = "a";
 
-
 function GameOver() {
-    // Switched to Highscore screen
+  // Switch to highscore screen
 
-    window.location.href = "./Highscores.html";
- }
+  window.location.href = "./Highscores.html";
+}
 
-// Startbtn.addEventListener("click", Countdown);
+// runs the timer
 Countdown();
 
 function Countdown() {
@@ -69,112 +66,69 @@ function Countdown() {
       timer.textContent = timeleft;
       timeleft--;
     }
-    // if else(timeleft = 0) {
-    //     alert("You've run out of time. Refresh and play again")
-    // }
- else {
+    // When timer ends go to Highscore page
+    else {
       timer.textContent = "0";
       GameOver();
     }
   }
- timeinterval = setInterval(setCountdown, 1000);
+  timeinterval = setInterval(setCountdown, 1000);
 }
 
-
 var CurrentQuestion = 0;
-
 
 StartQuiz();
 
 function StartQuiz() {
-    removeselected();
-    var currentQq = Quizquestions[CurrentQuestion];
-    questionEl.textContent = currentQq.question;
-    aanswer.textContent = currentQq.a;
-    banswer.textContent = currentQq.b;
-    canswer.textContent = currentQq.c;
-    danswer.textContent = currentQq.d;
-
+  removeselected();
+  var currentQq = Quizquestions[CurrentQuestion];
+  questionEl.textContent = currentQq.question;
+  aanswer.textContent = currentQq.a;
+  banswer.textContent = currentQq.b;
+  canswer.textContent = currentQq.c;
+  danswer.textContent = currentQq.d;
 }
-
+//removes preselected radio for next question
 function removeselected() {
-
-    answers.forEach((answers) => {
-        answers.checked = false;
-    });
+  answers.forEach((answers) => {
+    answers.checked = false;
+  });
 }
-
 
 function checkAnswer(userAnswer) {
+  // if answer is correct
+  if (userAnswer === Quizquestions[CurrentQuestion].correctanswer) {
+    console.log("correct answer");
+    score++;
+    scorecard.textContent = score;
+  } else {
+    timeleft = timeleft - 10;
+    if (timeleft > 0) {
+      timer.textContent = timeleft;
+    }
+  }
+  currentselectedanswer = "";
+}
 
-    // if answer is correct
-    if(userAnswer === Quizquestions[CurrentQuestion].correctanswer){
-        console.log('correct answer');
-        score++;
-        scorecard.textContent = score;
-    }
-    else {
-        timeleft = timeleft - 10;
-        if (timeleft > 0 ) {
-            timer.textContent = timeleft;
-        }
-    }
-     currentselectedanswer = "";
+answers.forEach((answerObject) => {
+  answerObject.addEventListener("click", (event) => {
+    currentselectedanswer = event.target.id;
+  });
+});
+submitbutton.addEventListener("click", (eventclear) => {
+  console.log(currentselectedanswer);
+
+  if (currentselectedanswer === "") {
+    return;
   }
 
-// function getSelected() {
-//     var answer3 = undefined;
-//     answers.forEach((answers) => {
-//         if(answers.checked) {
-//             return answers.id;
-//         }
-//         });
-//         return answer3;
-// }
+  checkAnswer(currentselectedanswer);
 
-answers.forEach( (answerObject) => {
-    
-    answerObject.addEventListener("click", (event) => {
-currentselectedanswer = event.target.id;
-
-    });
-    });
-
-
-
-
-submitbutton.addEventListener("click", (eventclear) => {
-console.log(currentselectedanswer);
-    // if(answers) {
-    //   if(answers === Quizquestions[CurrentQuestion].correctanswer) {
-    //       score++;
-    //   }
-    //   else {
-    //      timeleft = timeleft - 10;
-    //      timer.textContent = timeleft;
-    
-     if(currentselectedanswer === "") {
-         return;
-     }
-    
-
-    checkAnswer(currentselectedanswer);
-
-    CurrentQuestion++;
-    if(CurrentQuestion < Quizquestions.length) {
-
+  CurrentQuestion++;
+  if (CurrentQuestion < Quizquestions.length) {
     StartQuiz();
-    } else {
-        window.localStorage.setItem("key", score);
-        GameOver()
-    }
+  } else {
+    window.localStorage.setItem("key", score);
+    GameOver();
+  }
 });
-
-       //show the high score UI
-       //display the user scores
-       //input field for initials
-       //onclick for input initials submit
-       //when submitted insert/set local storage
-       //keep a list of scores in an array in local storage, each item will have the user initials and their score
-       //pend element in javascript, create new divs for each users score and their initials
-
